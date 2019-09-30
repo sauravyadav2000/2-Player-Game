b15a05d4eab74ad2819202e16a6937ad3ed4a3b8
sauravyadav2000 <sauravyadav764@gmail.com>
2019-09-30 22:17:15 +0530

    First Commit
    Added Unity Project

---
+++ 2 Player Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+    [SerializeField]
+    KeyCode keyLeft;
+    [SerializeField]
+    KeyCode keyRight;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyLeft))
+            GetComponent<Rigidbody>().velocity = v3Force;
+        if (Input.GetKey(keyRight)) 
+            GetComponent<Rigidbody>().velocity = -v3Force;
+    }
+}
---
+++ 2 Player Game/Assets/Scripts/PlayerMovement12.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement12 : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+    [SerializeField]
+    KeyCode keyUp;
+    [SerializeField]
+    KeyCode keyDown;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyUp)) 
+            GetComponent<Rigidbody>().velocity = v3Force;
+        if (Input.GetKey(keyDown)) 
+            GetComponent<Rigidbody>().velocity = -v3Force;
+    } 
+}
